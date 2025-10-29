e34f23da312dfb4aaa65cf81e12863bff18ae539
dpthenicest <fortuneprecious17@gmail.com>
2025-10-29 12:39:28 +0100

    completed data_types of python

--- data_types.py
+++ data_types.py
@@ -1,3 +1,6 @@
+# math module
+import math
+
 # String data type
 
 # literal assignment
@@ -80,3 +83,45 @@ print(first.count("o"))
 
 # Boolean data type
 myvalue = True
+x = bool(False)
+
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data types
+
+# Integer type
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+# float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+# complex type
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+
+# Built in function for numbers
+
+print(abs(gpa * -1))
+print(round(gpa))
+print(round(gpa, 1))
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a string to a number
+zipcode = "10001"
+zip_value = int(zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data
+zip_value = int("New York")
