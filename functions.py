e4df97a88d33627519c02a31d4d1be810f3000aa
dpthenicest <fortuneprecious17@gmail.com>
2025-10-30 00:08:08 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,24 @@
+def hello_world():
+  print("Hello World!")
+
+hello_world()
+
+def sum(num1=0, num2=0):
+  if type(num1) is not int or type(num2) is not int:
+    return 0
+  return num1 + num2
+
+total = sum()
+print(total)
+
+def multiple_items(*args):
+  print(args)
+  print(type(args)) # returns a tuple
+
+multiple_items("dave", "john", "sara")
+
+def multi_named_items(**kwargs):
+  print(kwargs)
+  print(type(kwargs)) # returns a dict
+
+multi_named_items(first = 'dave', last="gray")
