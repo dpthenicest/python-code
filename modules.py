a2b1588631ed6879d9f8ed8f28a72ea91bb78710
dpthenicest <fortuneprecious17@gmail.com>
2025-10-30 15:32:05 +0100

    fstrings, and modules

---
+++ modules.py
@@ -0,0 +1,20 @@
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kansas
+from rps7 import rock_paper_scissors
+
+print(f"{pi:.2f}")
+
+for item in dir(rdm):
+  print(item)
+
+print(kansas.capital)
+kansas.randomfunfact()
+
+print(__name__)
+print(kansas.__name__)
+print(kansas.bird)
+
+rock_paper_scissors()
